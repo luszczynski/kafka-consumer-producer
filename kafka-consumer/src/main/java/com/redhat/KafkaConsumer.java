ef848f7a7d9064f5ba47f8b1dda1153d2b5a5c5b
Gustavo Luszczynski <luszczynski@gmail.com>
2023-07-21 10:49:46 -0300

    add departamento

--- kafka-consumer/src/main/java/com/redhat/KafkaConsumer.java
+++ kafka-consumer/src/main/java/com/redhat/KafkaConsumer.java
@@ -3,6 +3,7 @@ package com.redhat;
 import jakarta.enterprise.context.ApplicationScoped;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.jboss.logging.Logger;
 
@@ -10,13 +11,18 @@ import org.jboss.logging.Logger;
 public class KafkaConsumer {
     private static final Logger LOGGER = Logger.getLogger(KafkaConsumer.class.getName());
 
+    @ConfigProperty(name = "departamento")
+    String departamento;
+
     @Incoming("kafka-in")
     public void consume(ConsumerRecord<String, KafkaMessage> record) {
-        String key = record.key();
-        KafkaMessage value = record.value(); 
-        String topic = record.topic();
-        int partition = record.partition();
-        LOGGER.infof("==========\nKey = %s\nPayload = %s\nDepartamento = %s\nTopic = %s\nPartition = %d\n==========\n\n", key, value.payload, value.departamento, topic, partition);
-        
+        KafkaMessage value = record.value();
+
+        if(value.departamento.equalsIgnoreCase(departamento)) {
+            String key = record.key();
+            String topic = record.topic();
+            int partition = record.partition();
+            LOGGER.infof("\n==========\nKey = %s\nPayload = %s\nDepartamento = %s\nTopic = %s\nPartition = %d\n==========\n\n", key, value.payload, value.departamento, topic, partition);
+        }
     }
 }
