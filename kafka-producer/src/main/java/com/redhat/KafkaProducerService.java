d5da6560702b8881ca1c6908fb4962059a4ef5d6
Gustavo Luszczynski <luszczynski@gmail.com>
2023-07-21 09:33:06 -0300

    fix bug

--- kafka-producer/src/main/java/com/redhat/KafkaProducerService.java
+++ kafka-producer/src/main/java/com/redhat/KafkaProducerService.java
@@ -6,29 +6,49 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.inject.Produces;
 
 public class KafkaProducerService {
-    private final KafkaProducerConfig config;
+   @ConfigProperty(name = "kafka.bootstrap.servers", defaultValue = "localhost:9092")
+    String bootstrapServers;
 
-    public KafkaProducerService(KafkaProducerConfig config) {
-        this.config = config;
-    }
+    @ConfigProperty(name = "kafka.acks", defaultValue = "all")
+    String acks;
+
+    @ConfigProperty(name = "kafka.retries", defaultValue = "0")
+    int retries;
+
+    @ConfigProperty(name = "kafka.batch.size", defaultValue = "16384")
+    int batchSize;
+
+    @ConfigProperty(name = "kafka.linger.ms", defaultValue = "1")
+    long lingerMs;
+
+    @ConfigProperty(name = "kafka.buffer.memory", defaultValue = "33554432")
+    long bufferMemory;
+
+    @ConfigProperty(name = "kafka.topic", defaultValue = "my-topic")
+    String topic;
+
+    @ConfigProperty(name = "kafka.compression.type", defaultValue = "lz4")
+    String compressionType;
+
 
     @Produces
     @ApplicationScoped
     public Producer<String, String> createProducer() {
         Properties props = new Properties();
 
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, config.getBootstrapServers());
-        props.put(ProducerConfig.ACKS_CONFIG, config.getAcks());
-        props.put(ProducerConfig.RETRIES_CONFIG, config.getRetries());
-        props.put(ProducerConfig.BATCH_SIZE_CONFIG, config.getBatchSize());
-        props.put(ProducerConfig.LINGER_MS_CONFIG, config.getLingerMs());
-        props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, config.getBufferMemory());
-        props.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, config.getCompressionType());
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ProducerConfig.ACKS_CONFIG, acks);
+        props.put(ProducerConfig.RETRIES_CONFIG, retries);
+        props.put(ProducerConfig.BATCH_SIZE_CONFIG, batchSize);
+        props.put(ProducerConfig.LINGER_MS_CONFIG, lingerMs);
+        props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, bufferMemory);
+        props.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, compressionType);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         
--- kafka-producer/src/main/java/com/redhat/KafkaProducerStartup.java
+++ kafka-producer/src/main/java/com/redhat/KafkaProducerStartup.java
@@ -2,6 +2,7 @@ package com.redhat;
 
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -11,6 +12,7 @@ import jakarta.annotation.PostConstruct;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 
+import java.util.Optional;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
@@ -22,16 +24,49 @@ public class KafkaProducerStartup {
     @Inject
     Producer<String, String> producer;
 
-    @Inject
-    KafkaProducerConfig config;
+    @ConfigProperty(name = "kafka.bootstrap.servers", defaultValue = "localhost:9092")
+    String bootstrapServers;
+
+    @ConfigProperty(name = "kafka.acks", defaultValue = "all")
+    String acks;
+
+    @ConfigProperty(name = "kafka.retries", defaultValue = "0")
+    int retries;
+
+    @ConfigProperty(name = "kafka.batch.size", defaultValue = "16384")
+    int batchSize;
+
+    @ConfigProperty(name = "kafka.linger.ms", defaultValue = "1")
+    long lingerMs;
+
+    @ConfigProperty(name = "kafka.buffer.memory", defaultValue = "33554432")
+    long bufferMemory;
+
+    @ConfigProperty(name = "kafka.topic", defaultValue = "my-topic")
+    String topic;
+
+    @ConfigProperty(name = "kafka.num.records", defaultValue = "512")
+    long numRecords;
+
+    @ConfigProperty(name = "kafka.record.size", defaultValue = "4096")
+    long recordSize;
+
+    @ConfigProperty(name = "kafka.throughput", defaultValue = "1000")
+    long throughput;
+
+    @ConfigProperty(name = "kafka.compression.type", defaultValue = "lz4")
+    String compressionType;
+
+    @ConfigProperty(name = "kafka.message.key")
+    String messageKey;
 
     private final Random random = new Random();
 
     @PostConstruct
     void produceRecords() {
         ObjectMapper objectMapper = new ObjectMapper();
-        IntStream.range(0, (int)config.getNumRecords()).forEach(i -> {
-            String key = config.getMessageKey() != null ? config.getMessageKey() : "key-" + i;
+        IntStream.range(0, (int)numRecords).forEach(i -> {
+            String key = messageKey != null ? messageKey : "key-" + i;
             KafkaMessage message = generateMessage();
             String value;
             try {
@@ -40,10 +75,10 @@ public class KafkaProducerStartup {
                 throw new RuntimeException(e);
             }
 
-            producer.send(new ProducerRecord<>(config.getTopic(), key, value));
+            producer.send(new ProducerRecord<>(topic, key, value));
 
-            if (config.getThroughput() > 0) {
-                long sleepTime = 1000 / config.getThroughput();
+            if (throughput > 0) {
+                long sleepTime = 1000 / throughput;
                 try {
                     TimeUnit.MILLISECONDS.sleep(sleepTime);
                 } catch (InterruptedException e) {
