8eb0bcd5e40234e858842a0b7e756a5e1c3c8c2f
Gustavo Luszczynski <luszczynski@gmail.com>
2023-07-21 10:05:04 -0300

    new consumer method

--- kafka-consumer/src/main/java/com/redhat/KafkaConsumer.java
+++ kafka-consumer/src/main/java/com/redhat/KafkaConsumer.java
@@ -4,6 +4,7 @@ import io.smallrye.reactive.messaging.annotations.Blocking;
 import io.smallrye.reactive.messaging.kafka.IncomingKafkaRecord;
 import jakarta.enterprise.context.ApplicationScoped;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.jboss.logging.Logger;
 
@@ -12,8 +13,18 @@ public class KafkaConsumer {
     private static final Logger LOGGER = Logger.getLogger(KafkaConsumer.class.getName());
 
     @Incoming("kafka-in")
-    @Blocking
-    public void onMessage(IncomingKafkaRecord<String, String> record) {
-        LOGGER.infof("Received from Kafka: Key = %s, Value = %s", record.getKey(), record.getPayload());
+    public void consume(ConsumerRecord<String, String> record) {
+        String key = record.key(); // Can be `null` if the incoming record has no key
+        String value = record.value(); // Can be `null` if the incoming record has no value
+        String topic = record.topic();
+        int partition = record.partition();
+        LOGGER.infof("Received from Kafka: Key = %s, Value = %s, Topic = %s, Partition = %d", key, value, topic, partition);
+        
     }
+
+    // @Incoming("kafka-in")
+    // @Blocking
+    // public void onMessage(IncomingKafkaRecord<String, String> record) {
+    //     LOGGER.infof("Received from Kafka: Key = %s, Value = %s", record.getKey(), record.getPayload());
+    // }
 }
